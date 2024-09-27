d32c473dece11f6729c2258e662f1720858888dd
Nonthakornn <ext.nonthakorn.che@truedigital.com>
2024-09-27 22:22:01 +0700

    27/09/2024 - Fix Error

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -6,11 +6,33 @@
 /*   By: nchencha <nchencha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 16:27:37 by nchencha          #+#    #+#             */
-/*   Updated: 2024/09/26 17:49:48 by nchencha         ###   ########.fr       */
+/*   Updated: 2024/09/27 22:09:26 by nchencha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_hex(unsigned long long n, const char c)
+{
+	int	count;
+
+	count = 0;
+	if (n >= 16)
+	{
+		count += ft_hex((n / 16), c);
+		count += ft_hex((n % 16), c);
+	}
+	else if (n > 9)
+	{
+		if (c == 'x')
+			count += ft_print_char(n + 87);
+		else if (c == 'X')
+			count += ft_print_char(n + 55);
+	}
+	else
+		count += ft_print_char(n + '0');
+	return (count);
+}
 
 int	ft_print_ptr(unsigned long long ptr)
 {
@@ -23,7 +45,7 @@ int	ft_print_ptr(unsigned long long ptr)
 		return (count);
 	}
 	count += write(1, "0x", 2);
-	count += ft_print_hex(ptr, 'x');
+	count += ft_hex(ptr, 'x');
 	return (count);
 }
 
